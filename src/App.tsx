46b23a52e3542046c3ecce5fbe5fdd2ed05c3db5
Joshua Varney <jrv123756@gmail.com>
2025-08-09 19:53:29 +0100

    Tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,15 @@ import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 
 function Homepage() {
     return(
-        <div>
-            <h1>Hello react in electron</h1>
-            <h1>Homepage</h1>
+        <div className="flex flex-col bg-white space-y-8 p-8">
+            <div className="flex flex-row bg-white space-x-8 p-8">
+                <h1 className="text-lg text-red-500">Hello react in electron</h1>
+                <h1 className="text-lg text-red-500">Homepage</h1>
+            </div>
+            <div className="flex flex-row bg-white space-x-8 p-8">
+                <h1 className="text-lg text-red-500">Hello react in electron</h1>
+                <h1 className="text-lg text-red-500">Homepage</h1>
+            </div>
         </div>
     )
 }
